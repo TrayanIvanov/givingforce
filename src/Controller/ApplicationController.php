e8e8ea324786fab9415ab0951f5615081f1ddde2
Trayan Ivanov <trayan.ivanov@gmail.com>
2021-10-31 19:21:59 +0200

    Create endpoint for progressing application

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -5,8 +5,13 @@ namespace App\Controller;
 use App\Entity\Application;
 use App\Form\ApplicationType;
 use App\Repository\ApplicationRepository;
+use App\Repository\UserRepository;
+use App\Service\ApplicationTransitioning;
+use App\Service\UserPermissions;
 use DateTime;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -45,5 +50,75 @@ class ApplicationController extends AbstractController
         return $this->renderForm('applications/create.html.twig', [
             'form' => $form,
         ]);
+    }
+
+    public function transit(
+        Request $request,
+        Application $application,
+        UserPermissions $userPermissions,
+        ApplicationTransitioning $applicationTransitioning,
+        UserRepository $userRepo
+    ): Response {
+        $transition = $request->attributes->get('transition');
+        $userId = $request->request->get('userId');
+
+        try {
+            $this->checkUser($userId, $userRepo, $userPermissions);
+            $this->checkTransition($transition, $applicationTransitioning, $application);
+            $newStage = $this->getNewStage($transition, $applicationTransitioning, $application);
+        } catch (Exception $e) {
+            return new JsonResponse($e->getMessage());
+        }
+
+        $application->setStage($newStage);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($application);
+        $entityManager->flush();
+
+        return new JsonResponse('Application transitioned.');
+    }
+
+    private function checkUser(string $userId, UserRepository $userRepo, UserPermissions $userPermissions): void
+    {
+        if (!$userId) {
+            throw new Exception('User not provided.');
+        }
+
+        $user = $userRepo->find($userId);
+
+        $isUserAllowed = $userPermissions->granted($user, UserPermissions::PERMISSION_MOVE_APPLICATION);
+        if (!$isUserAllowed) {
+            throw new Exception('User is not allowed to move applications.');
+        }
+    }
+
+    private function checkTransition(string $transition, ApplicationTransitioning $applicationTransitioning, Application $application): void
+    {
+        $transitioningAllowed = false;
+
+        if ($transition === ApplicationTransitioning::TRANSITION_FORWARD) {
+            $transitioningAllowed = $applicationTransitioning->transitForward($application);
+        }
+        if ($transition === ApplicationTransitioning::TRANSITION_BACKWARD) {
+            $transitioningAllowed = $applicationTransitioning->transitBackward($application);
+        }
+
+        if (!$transitioningAllowed) {
+            throw new Exception('Application can not be transitioned.');
+        }
+    }
+
+    private function getNewStage(string $transition, ApplicationTransitioning $applicationTransitioning, Application $application)
+    {
+        if ($transition === ApplicationTransitioning::TRANSITION_FORWARD) {
+            $newStage = $applicationTransitioning->nextStage($application->getStage());
+        }
+
+        if ($transition === ApplicationTransitioning::TRANSITION_BACKWARD) {
+            $newStage = $applicationTransitioning->previousStage($application->getStage());
+        }
+
+        return $newStage;
     }
 }
--- src/Service/ApplicationTransitioning.php
+++ src/Service/ApplicationTransitioning.php
@@ -6,6 +6,9 @@ use App\Entity\Application;
 
 class ApplicationTransitioning
 {
+    public const TRANSITION_FORWARD = 'forward';
+    public const TRANSITION_BACKWARD = 'backward';
+
     public function transitForward(Application $application): bool
     {
         return $application->getCreatedAt() < new \DateTime('-1 day')
@@ -17,5 +20,29 @@ class ApplicationTransitioning
     public function transitBackward(Application $application): bool
     {
         return $application->getCharity()->getCountry()->getCode() === 'GBR';
+    }
+
+    public function nextStage(string $stage): string
+    {
+        if ($stage === Application::STAGE_ORGANISATION_APROVAL) {
+            return Application::STAGE_ALLOW_TO_PROCEED;
+        }
+        if ($stage === Application::STAGE_ALLOW_TO_PROCEED) {
+            return Application::STAGE_PAID;
+        }
+
+        return '';
+    }
+
+    public function previousStage(string $stage): string
+    {
+        if ($stage === Application::STAGE_PAID) {
+            return Application::STAGE_ALLOW_TO_PROCEED;
+        }
+        if ($stage === Application::STAGE_ALLOW_TO_PROCEED) {
+            return Application::STAGE_ORGANISATION_APROVAL;
+        }
+
+        return '';
     }
 }
